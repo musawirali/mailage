4409d7583b2c50c9cf9a8e02fac9f116157108f5
Musawir Shah <musawir@gmail.com>
2016-04-01 16:38:17 -0700

    Initial Commit

---
+++ Mailage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Mailage
+//
+//  Created by Musawir Shah on 4/1/16.
+//  Copyright © 2016 YourMechanic. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
