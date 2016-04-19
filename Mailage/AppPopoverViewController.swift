8660d4d73c2b991270f3f91670717f675c6beecc
Musawir Shah <musawir@gmail.com>
2016-04-18 19:44:28 -0700

    Update: Add popover instead of menu

---
+++ Mailage/AppPopoverViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AppPopoverViewController.swift
+//  Mailage
+//
+//  Created by Musawir Shah on 4/18/16.
+//  Copyright © 2016 YourMechanic. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+class AppPopoverViewController: NSViewController {
+    
+}
